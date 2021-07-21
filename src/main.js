37d6cae84bf52ee371035217fef54f6bbc1ee839
ykyk3 <azz.ykoumoto@gmail.com>
2021-07-21 19:42:10 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,38 @@
+class Components extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.init();
+    }
+
+    // browser will call this when the element is removed from the document
+    disconnectedCallback() {}
+
+    static get observedAttributes() {
+        return ['data-state'];
+    }
+
+    // specifies the attributes to be monitored
+    attributeChangedCallback(name, old, value) {}
+
+    // Called when the element has been moved to a new document
+    // e.g. use case document.adoptNode
+    adoptedCallback() {}
+
+    init() {
+        this.addEvents();
+    }
+
+    addEvents() {
+        this.addEventListener('click', this.click);
+    }
+
+    click() {
+        this.dataset.state =
+            this.dataset.state === 'active' ? 'unactive' : 'active';
+    }
+}
+
+customElements.define('web-components', Components);
